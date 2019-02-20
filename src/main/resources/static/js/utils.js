f4d843901f0682ba86d9ecd96fc0926684c7608c
386959653 <386959653@qq.com>
2019-02-20 14:39:31 +0800

    1.更新utils.js

--- src/main/resources/static/js/utils.js
+++ src/main/resources/static/js/utils.js
@@ -66,6 +66,18 @@ var Utils = (function () {
         return ret;
     }
 
+    //深拷贝
+    function deepCopy(obj) {
+        if (typeof obj != 'object') {
+            return obj;
+        }
+        var newobj = {};
+        for (var attr in obj) {
+            newobj[attr] = deepCopy(obj[attr]);
+        }
+        return newobj;
+    }
+
     function isNum(parm) {
         return $.isNumeric(parm);
     }
@@ -273,7 +285,7 @@ var Utils = (function () {
     return {
         "isEmpty": isEmpty, "isNaN": isNaN, "isNotEmpty": isNotEmpty,
         "concat": concat, "mtConcat": mtConcat, "isAllEmpty": isAllEmpty,
-        "toMap": toMap, "clearFields": clearFields, "copy": copy,
+        "toMap": toMap, "clearFields": clearFields, "copy": copy, "deepCopy": deepCopy,
         "isEmail": isEmail, "isInt": isInt, "isDate": isDate, "isBetween": isBetween,
         "isURL": isURL, "isIP": isIP, "isPostalCode": isPostalCode,
         "isMobil": isMobil, "isTel": isTel, "isPassword": isPassword,
@@ -282,7 +294,6 @@ var Utils = (function () {
         "NumDescSort": NumDescSort, "NumAscSort": NumAscSort, "getByteLen": getByteLen
     };
 })();
-
 
 var AjaxHelper = (function () {
     function post(url, data, callback, async) {
@@ -326,7 +337,7 @@ var AjaxHelper = (function () {
                     + textStatus + "</div>" + "<div>thrownError:"
                     + thrownError + "</div>" + "<div>" + xhr.responseText
                     + "</div>")
-                $('#ajaxErrorModal').modal('toggle');
+                $('#errorModal').modal('toggle');
                 succeed = false;
             }
         });
@@ -337,3 +348,79 @@ var AjaxHelper = (function () {
         'post': post
     };
 })();
+
+
+/** * 对Date的扩展，将 Date 转化为指定格式的String * 月(M)、日(d)、12小时(h)、24小时(H)、分(m)、秒(s)、周(E)、季度(q)
+ 可以用 1-2 个占位符 * 年(y)可以用 1-4 个占位符，毫秒(S)只能用 1 个占位符(是 1-3 位的数字) * eg: * (new
+ Date()).pattern("yyyy-MM-dd hh:mm:ss.S")==> 2006-07-02 08:09:04.423
+ * (new Date()).pattern("yyyy-MM-dd E HH:mm:ss") ==> 2009-03-10 二 20:09:04
+ * (new Date()).pattern("yyyy-MM-dd EE hh:mm:ss") ==> 2009-03-10 周二 08:09:04
+ * (new Date()).pattern("yyyy-MM-dd EEE hh:mm:ss") ==> 2009-03-10 星期二 08:09:04
+ * (new Date()).pattern("yyyy-M-d h:m:s.S") ==> 2006-7-2 8:9:4.18
+ */
+Date.prototype.format = function (fmt) {
+    var o = {
+        "M+": this.getMonth() + 1, //月份
+        "d+": this.getDate(), //日
+        "h+": this.getHours() % 12 == 0 ? 12 : this.getHours() % 12, //小时
+        "H+": this.getHours(), //小时
+        "m+": this.getMinutes(), //分
+        "s+": this.getSeconds(), //秒
+        "q+": Math.floor((this.getMonth() + 3) / 3), //季度
+        "S": this.getMilliseconds() //毫秒
+    };
+    var week = {
+        "0": "/u65e5",
+        "1": "/u4e00",
+        "2": "/u4e8c",
+        "3": "/u4e09",
+        "4": "/u56db",
+        "5": "/u4e94",
+        "6": "/u516d"
+    };
+    if (/(y+)/.test(fmt)) {
+        fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    }
+    if (/(E+)/.test(fmt)) {
+        fmt = fmt.replace(RegExp.$1, ((RegExp.$1.length > 1) ? (RegExp.$1.length > 2 ? "/u661f/u671f" : "/u5468") : "") + week[this.getDay() + ""]);
+    }
+    for (var k in o) {
+        if (new RegExp("(" + k + ")").test(fmt)) {
+            fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+        }
+    }
+    return fmt;
+}
+
+/**
+ *js中更改日期
+ * y年， m月， d日， h小时， n分钟，s秒
+ */
+Date.prototype.add = function (part, value) {
+    value *= 1;
+    if (isNaN(value)) {
+        value = 0;
+    }
+    switch (part) {
+        case "y":
+            this.setFullYear(this.getFullYear() + value);
+            break;
+        case "m":
+            this.setMonth(this.getMonth() + value);
+            break;
+        case "d":
+            this.setDate(this.getDate() + value);
+            break;
+        case "h":
+            this.setHours(this.getHours() + value);
+            break;
+        case "n":
+            this.setMinutes(this.getMinutes() + value);
+            break;
+        case "s":
+            this.setSeconds(this.getSeconds() + value);
+            break;
+        default:
+
+    }
+}
